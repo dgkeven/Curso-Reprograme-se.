8f5ee780ba7bd7cdb33f3af299b01593c695b3c5
Keven Mendes <dgkeven@outlook.com>
2022-12-17 11:23:57 -0300

    Delete Semana_2 directory

--- Semana_2/Atividade Prática/controles.js
+++
@@ -1,7 +0,0 @@
-function contate_me() {
-    window.location.href = "https://web.whatsapp.com/send?text=Olá%20tudo%20bem???&phone=+5528999581746"
-}
-
-function send_me_notify() {
-    window.setTimeout('alert("Mensagem enviada com sucesso!!!");window.close();', 2000);
-}
