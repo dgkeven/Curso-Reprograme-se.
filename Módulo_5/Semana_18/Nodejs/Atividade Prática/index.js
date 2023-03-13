1e99e7dd71e12fb2c71c0ea5bfaf4693aa4cb1e7
Keven Mendes <dgkeven@outlook.com>
2023-03-12 23:06:47 -0300

    Add files via upload

---
+++ Módulo_5/Semana_18/Nodejs/Atividade Prática/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const saoPaulo = require('./rodizio');
+const datas = require('./data');
+const resultado = require('./divisao');
+
+saoPaulo.rodizio();
+datas.data();
+resultado.divisao();
+pessoas.ler_dados();
