ce38d9ad272087c8b7ef92ca15d7c1d356e0b9ef
Keven Mendes <dgkeven@outlook.com>
2023-03-22 12:10:41 -0300

    Add files via upload

---
+++ Módulo_5/Semana_19/Nodejs/Atividade Prática/index.js
@@ -0,0 +1,95 @@
+const prompt = require("prompt-sync")();
+const fs = require("fs");
+
+async function novo_carro() {
+  const placa = prompt("Digite a placa do carro: ");
+  const nome = prompt("Digite o nome do carro: ");
+  const montadora = prompt("Digite a montadora do carro: ");
+
+  const novoCarro = {
+    placa: placa,
+    nome: nome,
+    montadora: montadora,
+  };
+
+  try {
+    const carros = await obter_carros();
+    const ler_carros = JSON.parse(carros).carros;
+
+    await cadastrar_carro(ler_carros, novoCarro);
+    console.log("Carro adicionado com sucesso!!");
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function cadastrar_carro(ler_carros, novoCarro) {
+  ler_carros.push(novoCarro);
+  const json = JSON.stringify({ carros: ler_carros })
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./carros.json", json, (err) => {
+      if (err) {
+        reject(err)
+      }
+
+      resolve()
+    })
+  })
+}
+
+function obter_carros() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./carros.json", "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(data);
+    })
+  })
+}
+
+async function listar_carros() {
+  const carros = await obter_carros();
+  const ler_carros = JSON.parse(carros).carros;
+  console.table(ler_carros);
+}
+
+
+async function app() {
+  let opcao;
+
+  do {
+    console.log("Sistema de cadastro de carros\n");
+
+    console.log("1 - Listar carros");
+    console.log("2 - Cadastrar novo carro");
+    console.log("3 - Sair do sistema");
+
+    opcao = prompt("Escolha uma opção: ");
+
+    switch (opcao) {
+      case "1":
+        await listar_carros();
+        break;
+
+      case "2":
+        await novo_carro();
+        break;
+
+      case "3":
+        console.log("Obrigado por usar nosso sistema. Volte sempre.");
+        break;
+
+      default:
+        console.clear();
+        console.log("Opção inválida. Tente novamente.");
+        break;
+    }
+
+  } while (opcao !== "3");
+}
+
+app();
